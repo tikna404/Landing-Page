225df07a1f2537360c3102b481b4fefe4bc157f5
Ankit Pal <ankitp2104@gmail.com>
2024-06-15 11:49:31 +0530

    Add files via upload

---
+++ Medical_Landing page/script.js
@@ -0,0 +1,10 @@
+var menuitem = document.getElementById("menuitem");
+    menuitem.style.maxHeight = "0px";
+
+    function menutoggle() {
+      if (menuitem.style.maxHeight == '0px') {
+        menuitem.style.maxHeight = "200px"
+      } else {
+        menuitem.style.maxHeight = "0px"
+      }
+    }
